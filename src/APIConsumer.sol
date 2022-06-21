20b447258269db7baeb1d262009142d4200463ee
elcomendante <karol@finsane.com>
2022-06-21 11:41:21 +0100

    +

--- src/APIConsumer.sol
+++ src/APIConsumer.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.14;
 
-import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol"; 
+
 
 /**
  * @title The APIConsumer contract
